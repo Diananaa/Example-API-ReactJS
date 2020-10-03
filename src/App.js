c678614379eda80010e0f07f1831e3afb7116456
Diana <d1anasite04@gmail.com>
2020-10-03 23:47:54 +0700

    todo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component{
+  constructor(){
+    super();
+    this.state={todos: []}
+  }
+
+  addTodo=(e) =>{
+    e.preventDefault();
+
+    let jam = this.refs.jam.nodeValue;
+    let aktivitas = this.refs.aktivitas.value;
+
+    this.state.todos.push({jam, aktivitas});
+
+    this.setState({todos:this.state.todos});
+
+    this.refs.formulir.reset();
+    this.refs.jam.focus();
+  }
+
+  removeTodo = (i)=>{
+    this.state.todos.splice(i,1);
+    this.setState({todos: this.state.todos});
+  }
+
+  render(){
+    return(
+      <div>
+      <h3>Aplikasi Aktifitas Harian</h3>
+      <form ref="formulir">
+        <input type="text" ref="jam" placeholder="jam aktivitas"/>
+        <input type="text" ref="aktivitas" placeholder="jenis aktivitas"/>
+        <button onClick={this.addTodo}>Simpan</button>
+      </form>
+      <hr/>
+      <div>
+        <ul>
+          {this.state.todos.map((data,i)=>
+            <li key={i}>
+              <div>
+                <button onClick={()=>this.removeTodo(i)}>
+                  hapus</button> {data.jam} | {data.aktivitas}
+              </div>
+            </li>
+          )}
+        </ul>
+      </div>
+      </div>
+    );
+    }
+  }
 
 export default App;
+
